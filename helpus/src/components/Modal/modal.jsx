867af9143f0afa2c9b36504b4decbd137867d2f9
biankavm <bianka.maciel@jusbrasil.com.br>
2025-07-13 13:19:50 -0400

    feat: adjust styles of the modal

--- helpus/src/components/Modal/modal.jsx
+++ helpus/src/components/Modal/modal.jsx
@@ -1,54 +1,77 @@
 import styles from './modal.module.scss'
-import { FiX } from 'react-icons/fi'
+import { FiX, FiUser, FiFileText, FiCalendar, FiCircle } from 'react-icons/fi'
 import { handleColorStatus } from '../../shared'
 import commonStyles from '../../shared/common-styles.module.scss'
 
 export function Modal({ content, onClose }) {
   return (
-    <div className={styles.modal}>
-      <div className={styles.modalContainer}>
+    <div className={styles.modal} onClick={onClose}>
+      <div
+        className={styles.modalContainer}
+        onClick={(e) => e.stopPropagation()}
+      >
         <button className={styles.modalContainerClose} onClick={onClose}>
-          <FiX size={25} color="#fff" />
+          <FiX size={20} color="#fff" />
         </button>
 
         <main>
-          <h2> Detalhes do chamado</h2>
-          <div className={styles.modalRow}>
-            <span>
-              {' '}
-              Cliente: <i>{content.client}</i>
-            </span>
-          </div>
+          <h2>Detalhes do chamado</h2>
 
-          <div className={styles.modalRow}>
-            <span>
-              {' '}
-              Assunto: <i>{content.subject}</i>
-            </span>
-          </div>
+          <div className={styles.modalInfoCard}>
+            <div className={styles.modalRow}>
+              <div className={`${styles.modalIcon} ${styles.client}`}>
+                <FiUser />
+              </div>
+              <div>
+                <span className={styles.modalLabel}>Cliente</span>
+                <div className={styles.modalValue}>{content.client}</div>
+              </div>
+            </div>
 
-          <div className={styles.modalRow}>
-            <span>
-              {' '}
-              Cadastrado em: <i>{content.createdFormatted}</i>
-            </span>
-          </div>
+            <div className={styles.modalRow}>
+              <div className={`${styles.modalIcon} ${styles.subject}`}>
+                <FiFileText />
+              </div>
+              <div>
+                <span className={styles.modalLabel}>Assunto</span>
+                <div className={styles.modalValue}>{content.subject}</div>
+              </div>
+            </div>
 
-          <div className={styles.modalRow}>
-            <span>
-              {' '}
-              Status:{' '}
-              <i className={handleColorStatus(content.status, commonStyles)}>
-                {content.status}
-              </i>
-            </span>
+            <div className={styles.modalRow}>
+              <div className={`${styles.modalIcon} ${styles.date}`}>
+                <FiCalendar />
+              </div>
+              <div>
+                <span className={styles.modalLabel}>Cadastrado em</span>
+                <div className={styles.modalValue}>
+                  {content.createdFormatted}
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.modalRow}>
+              <div className={`${styles.modalIcon} ${styles.status}`}>
+                <FiCircle />
+              </div>
+              <div>
+                <span className={styles.modalLabel}>Status</span>
+                <div
+                  className={`${styles.modalValue} ${
+                    styles.status
+                  } ${handleColorStatus(content.status, commonStyles)}`}
+                >
+                  {content.status}
+                </div>
+              </div>
+            </div>
           </div>
 
           {content.complement && (
-            <>
-              <h3> Complemento </h3>
+            <div className={styles.modalComplement}>
+              <h3>Complemento</h3>
               <p>{content.complement}</p>
-            </>
+            </div>
           )}
         </main>
       </div>
